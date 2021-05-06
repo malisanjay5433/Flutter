9e77d94d2e93ec7ce2e440d0ab36bd093801918c
= <malisanjay5433@gmail.com>
2021-05-06 17:25:32 +0530

    Login and Routes Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter1/pages/home_page.dart';
 import 'package:flutter1/pages/login_page.dart';
+import 'package:flutter1/untils/routes.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,12 +13,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      themeMode:ThemeMode.dark,
+      themeMode:ThemeMode.light,
+      theme:ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily:GoogleFonts.lato().fontFamily,
+        primaryTextTheme: GoogleFonts.latoTextTheme()),
+      darkTheme:ThemeData(
+        brightness:Brightness.dark,
+      ),
       // home:HomePage()
       initialRoute: "/",
       routes:{
-            "/" : (context) => LoginPage(),
-            "/home":(context) => HomePage(),
+          MyRoutes.loginRoute : (context) => LoginPage(),
+          MyRoutes.homeRoute:(context) => HomePage(),
       },
     );
   }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter1/untils/routes.dart';
 
 class LoginPage extends StatelessWidget {
   @override
@@ -7,41 +8,44 @@ class LoginPage extends StatelessWidget {
       color: Colors.white,
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
-        child: Column(
-          children: [
-            Image.asset(
-              'assets/images/login_image.png',
-              fit: BoxFit.cover,
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              "Welcome to login Page",
-              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 20),
-            Column(children: [
-              TextFormField(
-                decoration: InputDecoration(
-                    hintText: "Enter UserName", labelText: "UserName"),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset(
+                'assets/images/login_image.png',
+                fit: BoxFit.cover,
+                // height: 500,
               ),
-              TextFormField(
-                obscureText:true,
-                decoration: InputDecoration(
-                    hintText: "Enter Password", labelText: "Password"),
+              SizedBox(
+                height: 20,
               ),
-              SizedBox(height:20),
-             ElevatedButton(
-               child: Text("Login"),
-                style:TextButton.styleFrom(),
-               onPressed:(){
-                 print("Login Button");
-               },
-             )
-            ]
-            )
-          ],
+              Text(
+                "Welcome to login",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 20),
+              Column(children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                      hintText: "Enter UserName", labelText: "UserName"),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      hintText: "Enter Password", labelText: "Password"),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  child: Text("Login"),
+                  style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+                  onPressed: () {
+                    // print("Login Button");
+                    Navigator.pushNamed(context, MyRoutes.homeRoute);
+                  },
+                )
+              ])
+            ],
+          ),
         ),
       ),
     );
