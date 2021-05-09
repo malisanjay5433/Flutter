b0c708f8447bc9a554f701c4299076c42b3b34e0
= <malisanjay5433@gmail.com>
2021-05-09 13:56:33 +0530

    day11 completed

---
+++ lib/pages/home_page 2.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter1/widgets/drawer.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:Text('Catalog App'),
+      ),
+      body: Center(
+        child: Container(
+          child: Text('Welcome to 30 days of Flutter'),
+        ),
+      ),
+      drawer:MyDrawer(),
+    );
+  }
+}
