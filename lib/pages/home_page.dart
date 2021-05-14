ff07832dda4335ea6165df20ba3e52a07374c8f8
= <malisanjay5433@gmail.com>
2021-05-14 18:52:17 +0530

    day 14 15 16 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -39,13 +39,42 @@ class _HomePageState extends State<HomePage> {
       body: Padding(
           padding: const EdgeInsets.all(16.0),
           child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
-              ? ListView.builder(
-                  itemCount: CatalogModel.items.length,
+              ? GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 16,
+                      crossAxisSpacing: 16),
                   itemBuilder: (context, index) {
-                    return ItemWidget(
-                      item: CatalogModel.items[index],
+                    final item = CatalogModel.items[index];
+                    return Card(
+                      clipBehavior: Clip.antiAlias,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      child: GridTile(
+                        header: Container(
+                          child: Text(
+                            item.name,
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          padding: const EdgeInsets.all(10),
+                          decoration: BoxDecoration(color: Colors.deepPurple),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Image.network(item.image),
+                        ),
+                        footer: Container(
+                          child: Text(
+                            item.price.toString(),
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          padding: const EdgeInsets.all(10),
+                          decoration: BoxDecoration(color: Colors.deepPurple),
+                        ),
+                      ),
                     );
-                  })
+                  },
+                  itemCount: CatalogModel.items.length)
               : Center(
                   child: CircularProgressIndicator(),
                 )),
