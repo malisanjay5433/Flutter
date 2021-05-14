84f919e768c5184369008dda29a7bc5ca8ff34a7
= <malisanjay5433@gmail.com>
2021-05-14 12:45:33 +0530

    day 12 Completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,19 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter1/models/catalog.dart';
 import 'package:flutter1/widgets/drawer.dart';
+import 'package:flutter1/widgets/item.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final dataSourceList = List.generate(50, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
-        title:Text('Catalog App'),
+        title: Text('Catalog App'),
       ),
-      body: Center(
-        child: Container(
-          child: Text('Welcome to 30 days of Flutter'),
-        ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+            itemCount: dataSourceList.length,
+            itemBuilder: (context, index) {
+              return ItemWidget(
+                item: dataSourceList[index],
+              );
+            }),
       ),
-      drawer:MyDrawer(),
+      drawer: MyDrawer(),
     );
   }
 }
